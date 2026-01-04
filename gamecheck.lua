4f40074b2b7b7c2ddf90d0a76774c9aa9534584b
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2026-01-04 16:52:26 -0600

    Update kick message for unauthorized players

--- gamecheck.lua
+++ gamecheck.lua
@@ -2,7 +2,7 @@ local ALLOWED_PLACE = 16261605398
 
 if game.PlaceId ~= ALLOWED_PLACE then
     for _, player in ipairs(game.Players:GetPlayers()) do
-        player:kick('Airsoft Battles')
+        player:kick('Script Only Works In Airsoft Battles')
     end
     return
 end
